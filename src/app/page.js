c2fbce8b42cb61552f678c97cc63b9e682ecb53c
Vishal Srivastava <eth.vishal@gmail.com>
2025-07-16 17:14:27 +0530

    Minor UI fixes.

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,18 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   return (
     <div>
-      <div className="grid md:grid-cols-10">
-        <div className="col-span-2 py-5 px-5 min-w-fit" onClick={()=>setLoading(true)}>
+      <div className="flex justify-between">
+        <div className="py-5 px-5 min-w-fit" onClick={()=>setLoading(true)}>
           <Link href={"/convert"}>
             <div className="p-8 border-1 border-slate-300 rounded-4xl hover:bg-gray-900">
               <h1 className="text-white text-3xl font-semibold">
                 Krutidev to Unicode Converter
               </h1>
-              <ul className="mt-5 space-y-2 text-base mb-2 min-w-fit w-full">
-                <li>🎡 &nbsp; Retains the format including table. </li>
-                <li>🚩 &nbsp; Hassle free Conversion</li>
+              <ul className="mt-5 space-y-2 text-sm mb-2 min-w-fit w-full">
+                <li>🎡 &nbsp; Retains the format (bold, italic, underline, table etc.) </li>
                 <li>🚀 &nbsp; 100% Accurate and fast</li>
-                <li>🤷‍♂️ &nbsp; Easy to use</li>
+                <li>🚩 &nbsp; Hassle free Conversion</li>
+                <li>🤷‍♂️ &nbsp; Absolutely free</li>
               </ul>
               
               <span className="flex justify-end">
@@ -39,7 +39,7 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className="col-span-8"></div>
+        <div className=""></div>
       </div>
     </div>
   );
