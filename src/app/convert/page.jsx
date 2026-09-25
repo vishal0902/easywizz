18d82ff6a0c15a8fcd56db4004105f5292828a70
Vishal Srivastava <vishal.sri0902@gmail.com>
2026-09-25 12:04:35 +0530

    fix: error page for convert page

--- src/app/convert/page.jsx
+++ src/app/convert/page.jsx
@@ -1,4 +1,7 @@
 "use client"
+import { headers } from "next/headers";
+import { notFound } from "next/navigation";
+
 
 import { useSession } from "next-auth/react";
 // import { motion } from "motion/react"
@@ -7,7 +10,17 @@ import TiptapEditor from "../components/TiptapEditor";
 
 
 
-export default function Page() {
+export default async function Page() {
+
+  //new code
+  const headerList = await headers();
+  const host = headerList.get("host") || "";
+
+  if (host === "easywizz.vercel.app" || host.endsWith(".vercel.app") || host.endsWith(".vercel.app/convert")) {
+    notFound();
+  }
+
+
   const {data: session} = useSession()
   if(!session){
     redirect("/api/auth/signin")
